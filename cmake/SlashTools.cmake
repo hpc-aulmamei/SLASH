6a5cddc99095245d505e419c1a7c273612b30cb4
Vlad Gabriel Serbu <Vlad-Gabriel.Serbu@amd.com>
2026-03-19 15:50:39 +0000

    Removed IP_REPO from build examples to align with linker updates
    Signed-off-by: Vlad Gabriel Serbu <Vlad-Gabriel.Serbu@amd.com>

--- cmake/SlashTools.cmake
+++ cmake/SlashTools.cmake
@@ -67,7 +67,7 @@ endif()
 set(SLASH_FOUND TRUE)
 
 function(add_vbin)
-    set(oneValueArgs TARGET CFG PLATFORM IP_REPO)
+    set(oneValueArgs TARGET CFG PLATFORM)
     set(multiValueArgs KERNELS)
     cmake_parse_arguments(SLASH_VBIN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
@@ -94,7 +94,6 @@ function(add_vbin)
                 "-p" "${SLASH_VBIN_PLATFORM}"
                 "-o" "${SLASH_VBIN_FILE}"
                 "-k" ${SLASH_VBIN_KERNELS}
-                "--ip-repository" "${SLASH_VBIN_IP_REPO}"
                 "--vivado" "${VIVADO_BINARY}"
             BYPRODUCTS "${SLASH_VBIN_FILE}.prj"
             DEPENDS "${SLASH_VBIN_CFG}" "${SLASH_VBIN_KERNELS}"
@@ -109,7 +108,6 @@ function(add_vbin)
                 "-p" "${SLASH_VBIN_PLATFORM}"
                 "-o" "${SLASH_VBIN_FILE}"
                 "-k" ${SLASH_VBIN_KERNELS}
-                "--ip-repository" "${SLASH_VBIN_IP_REPO}"
                 "--vivado" "${VIVADO_BINARY}"
             BYPRODUCTS "${SLASH_VBIN_FILE}.prj"
             DEPENDS "${SLASH_VBIN_CFG}" "${SLASH_VBIN_KERNELS}"
